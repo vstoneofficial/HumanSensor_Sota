7e39c2f712a7c735071607efb613a06892a7790d
fukatsu <fukatsu@vstone.co.jp>
2020-02-06 09:49:51 +0900

    第3回目

--- lib/usb/Hiddata.java
+++ lib/usb/Hiddata.java
@@ -7,10 +7,29 @@ public class Hiddata {
 	public native int usbhidSetReport(int device, byte[] buffer, int len);
 	public native int usbhidGetReport(int device, int reportID,byte[] buffer,int len);
 	
-	public static void main(String[] args) {
-		System.loadLibrary("Hiddata");
-		Hiddata hiddata = new Hiddata();
-		hiddata.usbhidOpenDevice(0,0);
-		hiddata.usbhidCloseDevice(0);
-	}
+  	public static void main(String[] args) {
+    	System.loadLibrary("Hiddata");
+
+	    try{
+			Hiddata hiddata = new Hiddata();
+			int dev = hiddata.usbhidOpenDevice(0x16c0,0x05df);
+			if(dev==0){
+				System.out.println("Sensor not found.");
+			}
+			else{
+				System.out.println("Sensor found.");
+				for(int i=0;i<30;i++){
+					byte[] buffer = new byte[17];
+					if(hiddata.usbhidGetReport(dev,0,buffer ,buffer.length)==0){
+						System.out.println("value:"+buffer[1]);
+					}
+					else System.out.println("Sensor read error.");
+					Thread.sleep(500);
+				}
+				hiddata.usbhidCloseDevice(dev);
+			}
+		} catch(InterruptedException e){
+      		e.printStackTrace();
+    	} 
+  	}
 }
--- lib_usb_Hiddata.c
+++ lib_usb_Hiddata.c
@@ -1,25 +1,226 @@
 #include "lib_usb_Hiddata.h"
+#include <stdio.h>
+#include "hiddata.h"
+#include <string.h>
+#include <usb.h>
+
 JNIEXPORT jint JNICALL Java_lib_usb_Hiddata_usbhidOpenDevice
 (JNIEnv *env, jobject thisj, jint vendor, jint product)
 {
-  printf("Called usbhidOpenDevice\n");
-  return 0;
+  usbDevice_t     *dev = NULL;
+  if (usbhidOpenDevice(&dev, vendor, NULL, product, NULL, NULL, 0) != 0) {
+    return 0;
+  }
+  return (int)dev;
 }
+
 JNIEXPORT void JNICALL Java_lib_usb_Hiddata_usbhidCloseDevice
 (JNIEnv *env, jobject thisj, jint device)
 {
-  printf("Called usbhidCloseDevice\n");
-  return ;
+  usbhidCloseDevice((usbDevice_t*) device);
 }
+
 JNIEXPORT jint JNICALL Java_lib_usb_Hiddata_usbhidSetReport
 (JNIEnv *env, jobject thisj, jint device, jbyteArray buffer, jint len)
 {
-  printf("Called usbhidSetReport\n");
-  return 0;
+  int returnValue;
+  int i;
+  jsize size = (*env)->GetArrayLength( env, buffer);
+  jbyte *buf = (*env)->GetByteArrayElements(env, buffer, 0);
+  char *sendbuf = malloc(size);
+  for (i = 0; i < size; i++) sendbuf[i]= buf[i];
+  returnValue = usbhidSetReport((usbDevice_t*)device, sendbuf,len);
+  free(sendbuf);
+  return returnValue;
 }
+
 JNIEXPORT jint JNICALL Java_lib_usb_Hiddata_usbhidGetReport
 (JNIEnv *env, jobject thisj, jint device, jint reportID, jbyteArray buffer, jint len)
 {
-  printf("Called usbhidGetReport\n");
-  return 0;
+  int returnValue;
+  jbyte *buf = (*env)->GetByteArrayElements(env, buffer, 0);
+  int _len = len;
+  char *recvbuf = malloc(_len);
+  int i;
+  returnValue = usbhidGetReport((usbDevice_t*)device, reportID, recvbuf, &_len);
+  if (returnValue==0) {
+    for (i = 0; i < _len; i++) buf[i] = recvbuf[i];
+  }
+  (*env)->ReleaseByteArrayElements(env, buffer,buf, 0);
+  free(recvbuf);
+  return returnValue;
+}
+
+//ここまでが現在のlib_usb_Hiddata.cの内容。
+//以降が、hiddata.cの12行目以降からコピーした内容
+
+#define usbDevice   usb_dev_handle  /* use libusb's device structure */
+
+/* ------------------------------------------------------------------------- */
+
+#define USBRQ_HID_GET_REPORT    0x01
+#define USBRQ_HID_SET_REPORT    0x09
+
+#define USB_HID_REPORT_TYPE_FEATURE 3
+
+
+static int  usesReportIDs;
+
+/* ------------------------------------------------------------------------- */
+
+static int usbhidGetStringAscii(usb_dev_handle *dev, int index, char *buf, int buflen)
+{
+  char    buffer[256];
+  int     rval, i;
+  
+  if((rval = usb_get_string_simple(dev, index, buf, buflen)) >= 0) /* use libusb version if it works */
+    return rval;
+  if((rval = usb_control_msg(dev, USB_ENDPOINT_IN, USB_REQ_GET_DESCRIPTOR, (USB_DT_STRING << 8) + index, 0x0409, buffer, sizeof(buffer), 5000)) < 0)
+    return rval;
+  if(buffer[1] != USB_DT_STRING){
+    *buf = 0;
+    return 0;
+  }
+  if((unsigned char)buffer[0] < rval)
+    rval = (unsigned char)buffer[0];
+  rval /= 2;
+  /* lossy conversion to ISO Latin1: */
+  for(i=1;i<rval;i++){
+    if(i > buflen)              /* destination buffer overflow */
+      break;
+    buf[i-1] = buffer[2 * i];
+    if(buffer[2 * i + 1] != 0)  /* outside of ISO Latin1 range */
+      buf[i-1] = '?';
+  }
+  buf[i-1] = 0;
+  return i-1;
+}
+
+int usbhidOpenDevice(usbDevice_t **device, int vendor, char *vendorName, int product, char *productName, char *serialNumber, int _usesReportIDs)
+{
+struct usb_bus      *bus;
+struct usb_device   *dev;
+usb_dev_handle      *handle = NULL;
+int                 errorCode = USBOPEN_ERR_NOTFOUND;
+static int          didUsbInit = 0;
+
+    if(!didUsbInit){
+        usb_init();
+        didUsbInit = 1;
+    }
+    usb_find_busses();
+    usb_find_devices();
+    for(bus=usb_get_busses(); bus; bus=bus->next){
+        for(dev=bus->devices; dev; dev=dev->next){
+            if(dev->descriptor.idVendor == vendor && dev->descriptor.idProduct == product){
+                char    string[256];
+                int     len;
+                handle = usb_open(dev); /* we need to open the device in order to query strings */
+                if(!handle){
+                    errorCode = USBOPEN_ERR_ACCESS;
+                    fprintf(stderr, "Warning: cannot open USB device: %s\n", usb_strerror());
+                    continue;
+                }
+                if(vendorName == NULL && productName == NULL){  /* name does not matter */
+                    break;
+                }
+                /* now check whether the names match: */
+                len = usbhidGetStringAscii(handle, dev->descriptor.iManufacturer, string, sizeof(string));
+                if(len < 0){
+                    errorCode = USBOPEN_ERR_IO;
+                    fprintf(stderr, "Warning: cannot query manufacturer for device: %s\n", usb_strerror());
+                }else{
+                    errorCode = USBOPEN_ERR_NOTFOUND;
+                    /* fprintf(stderr, "seen device from vendor ->%s<-\n", string); */
+                    if(strcmp(string, vendorName) == 0){
+                        len = usbhidGetStringAscii(handle, dev->descriptor.iProduct, string, sizeof(string));
+                        if(len < 0){
+                            errorCode = USBOPEN_ERR_IO;
+                            fprintf(stderr, "Warning: cannot query product for device: %s\n", usb_strerror());
+                        }else{
+                            errorCode = USBOPEN_ERR_NOTFOUND;
+                            /* fprintf(stderr, "seen product ->%s<-\n", string); */
+                            if(strcmp(string, productName) == 0) {
+			      
+			      len = usbhidGetStringAscii(handle, dev->descriptor.iSerialNumber, string, sizeof(string));
+			      if( len < 0 ) {
+				errorCode = USBOPEN_ERR_IO;
+				fprintf(stderr, "Warning: cannot query product for device: %s\n", usb_strerror());
+			      } else {
+				errorCode = USBOPEN_ERR_NOTFOUND;
+				if ( serialNumber == NULL ) {
+				  fprintf(stdout, "%s\n", string);
+				} else if( strcmp(serialNumber, "ANY") == 0 ) {
+				  break;
+				} else {
+				  if( strcmp(string, serialNumber) == 0 ) break;
+				}
+			      }
+			    }
+                        }
+                    }
+                }
+                usb_close(handle);
+                handle = NULL;
+            }
+        }
+        if(handle)
+            break;
+    }
+    if(handle != NULL){
+        errorCode = 0;
+        *device = (void *)handle;
+        usesReportIDs = _usesReportIDs;
+    }
+    return errorCode;
+}
+
+/* ------------------------------------------------------------------------- */
+
+void    usbhidCloseDevice(usbDevice_t *device)
+{
+    if(device != NULL)
+        usb_close((void *)device);
+}
+
+/* ------------------------------------------------------------------------- */
+
+int usbhidSetReport(usbDevice_t *device, char *buffer, int len)
+{
+int bytesSent, reportId = buffer[0];
+
+    if(!usesReportIDs){
+        buffer++;   /* skip dummy report ID */
+        len--;
+    }
+    bytesSent = usb_control_msg((void *)device, USB_TYPE_CLASS | USB_RECIP_DEVICE | USB_ENDPOINT_OUT, USBRQ_HID_SET_REPORT, USB_HID_REPORT_TYPE_FEATURE << 8 | (reportId & 0xff), 0, buffer, len, 5000);
+    if(bytesSent != len){
+        if(bytesSent < 0)
+            fprintf(stderr, "Error sending message: %s\n", usb_strerror());
+        return USBOPEN_ERR_IO;
+    }
+    return 0;
+}
+
+/* ------------------------------------------------------------------------- */
+
+int usbhidGetReport(usbDevice_t *device, int reportNumber, char *buffer, int *len)
+{
+int bytesReceived, maxLen = *len;
+
+    if(!usesReportIDs){
+        buffer++;   /* make room for dummy report ID */
+        maxLen--;
+    }
+    bytesReceived = usb_control_msg((void *)device, USB_TYPE_CLASS | USB_RECIP_DEVICE | USB_ENDPOINT_IN, USBRQ_HID_GET_REPORT, USB_HID_REPORT_TYPE_FEATURE << 8 | reportNumber, 0, buffer, maxLen, 5000);
+    if(bytesReceived < 0){
+        fprintf(stderr, "Error sending message: %s\n", usb_strerror());
+        return USBOPEN_ERR_IO;
+    }
+    *len = bytesReceived;
+    if(!usesReportIDs){
+        buffer[-1] = reportNumber;  /* add dummy report ID */
+        (*len)++;
+    }
+    return 0;
 }
